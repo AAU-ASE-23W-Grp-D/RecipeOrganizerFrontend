0176b1d9a071d7279278217f89b39b84a2500851
marleo <mariole@edu.aau.at>
2023-12-04 20:47:36 +0100

    Added Mockito for future testing (network mocking) Restructured Project (split main file and added respective folders for future widgets) Added basic test, checking if all input fields/buttons on login are present

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'screens/login_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe Organizer',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(title: 'Recipe Organizer'),
+    );
+  }
+}
---
+++ test/login_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:recipe_organizer_frontend/app.dart';
+
+void main() {
+  testWidgets('Check input fields and submit button', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our app has a username field.
+    expect(find.byKey(const Key('usernameField')), findsOneWidget);
+
+    // Verify that our app has a password field.
+    expect(find.byKey(const Key('passwordField')), findsOneWidget);
+
+    // Verify that our app has a submit button.
+    expect(find.byKey(const Key('loginButton')), findsOneWidget);
+  });
+}
