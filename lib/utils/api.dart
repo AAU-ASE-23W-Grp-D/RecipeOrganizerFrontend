a88b7d226e12c02d080297d68982e8579f82b38a
Christian Schönberg <1726822+chrisch1974@users.noreply.github.com>
2023-12-29 09:26:05 +0100

    #2 Handle new signin logic and store jwt token locally

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:recipe_organizer_frontend/utils/token_storage.dart';
 import 'package:recipe_organizer_frontend/screens/new_screen.dart';
 
 Future<void> login(String username, String password, BuildContext context) async {
+  final TokenStorage storage = TokenStorage();
   final response = await http.post(
     //TODO: CHANGE IP TO YOUR IP
     Uri.parse('http://192.168.0.11:8080/api/login'),
@@ -16,9 +18,24 @@ Future<void> login(String username, String password, BuildContext context) async
     }),
   );
 
-  if (response.statusCode == 200 && response.body == 'success') {
+  if (response.statusCode == 200) {
     // Login successful, handle accordingly (navigate to the next screen, etc.)
     print('Login successful');
+
+    Map<String, dynamic> body = json.decode(response.body);
+    int userId = body['id'];
+    String username = body['username'];
+    List<String> roles = List<String>.from(body['roles']);
+    String token = body['token'];
+
+    print('User ID: $userId');
+    print('Username: $username');
+    print('Roles: $roles');
+    print('Token: $token');
+
+    // Store JWT token
+    await storage.saveToken(token);
+
     _navigateToNextScreen(context);
   } else {
     // Login failed, handle accordingly (show error message, etc.)
