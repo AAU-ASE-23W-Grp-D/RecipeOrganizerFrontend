02b49b0c202a260f0b1667e9c30ba35dd15a2769
Stefan Schellander <stefansche@edu.aau.at>
2024-01-10 12:16:20 +0100

    Changed tests for registration page

--- test/registration_test.dart
+++ test/registration_test.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:recipe_organizer_frontend/screens/registration_page.dart';
+import 'package:recipe_organizer_frontend/app.dart';
 
 void main() {
   testWidgets('Check input fields and register button', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Registration());
+    await tester.pumpWidget(const MyApp());
+
+    // Click on the registration button
+    await tester.tap(find.byKey(Key('registrationButton')));
+    await tester.pumpAndSettle();
 
     // Verify that our app has a username field.
     expect(find.byKey(const Key('usernameFieldRegistration')), findsOneWidget);
