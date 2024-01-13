9c29e824a00d2554cdd04c25c4daf3da0f555839
SMoserGH <s2moser@edu.aau.at>
2024-01-13 17:38:13 +0100

    Issue #6, #19 and #12: updated home page, liked recipes and profile page

--- lib/screens/liked_recipes_screen.dart
+++ lib/screens/liked_recipes_screen.dart
@@ -25,51 +25,53 @@ class LikedRecipesPage extends StatelessWidget {
         title: const Text('My Liked Recipes'),
       ),
 
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage(profile.profileImage),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Text(
-              profile.name,
-              style: const TextStyle(fontSize: 24),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Text(
-                'Liked Recipes: ${profile.likedRecipes}',
-                style: const TextStyle(fontSize: 12),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+        
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(profile.profileImage),
               ),
-          ),
-
-          const Divider(
-            thickness: 2,
-          ),
-
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              'Liked Recipes',
-              style: TextStyle(fontSize: 18),
             ),
-          ),
-
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: GridB(),
-          )
-        ]
+            Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Text(
+                profile.name,
+                style: const TextStyle(fontSize: 24),
+              ),
+            ),
+        
+            Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Text(
+                  'Liked Recipes: ${profile.likedRecipes}',
+                  style: const TextStyle(fontSize: 12),
+                ),
+            ),
+        
+            const Divider(
+              thickness: 2,
+            ),
+        
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Liked Recipes',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+        
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: GridB(),
+            )
+          ]
+        ),
       )
     );
   }
