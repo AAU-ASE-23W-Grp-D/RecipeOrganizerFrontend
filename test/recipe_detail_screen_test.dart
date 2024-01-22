4034d0a947653b48d84e68fbfb647130171b04ac
SMoserGH <s2moser@edu.aau.at>
2024-01-22 18:24:05 +0100

    Issue #7: created tests for recipe screen

--- test/recipe_detail_screen_test.dart
+++ test/recipe_detail_screen_test.dart
@@ -1,41 +1,39 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:recipe_organizer_frontend/models/recipe.dart';
 import 'package:recipe_organizer_frontend/screens/recipe_detail_screen.dart';
 import 'package:network_image_mock/network_image_mock.dart';
 void main() {
+  late Uint8List imageData;
+  late Recipe recipe;
+
+  setUp(() async {
+    final imageFile = File('assets/test_resources/image.png');
+    imageData = await imageFile.readAsBytes();
+
+    recipe = Recipe(ID: 1, name: 'Recipe 1', image: imageData, ingredients: '1*Ei, 1*Ei', description: 'Description 1', rating: 5, rating_amount: 1);
+  });
   //setUpAll(() => HttpOverrides.global = null);
-  /*testWidgets('RecipeDetailScreenWeb should render', (WidgetTester tester) async {
+  testWidgets('RecipeDetailScreenWeb should render', (WidgetTester tester) async {
     // Build our widget and trigger a frame.
-    mockNetworkImagesFor(() => tester.pumpWidget(
+
+    await tester.pumpWidget(
       MaterialApp(
-        //home: RecipeDetailScreenWeb(recipe: null,),
+        home: RecipeDetailScreenWeb(recipe: recipe),
       ),
-    ));
+    );
 
     // Wait for the network images to load
     
       // Verify if the widget renders correctly.
       expect(find.text('Description'), findsOneWidget);
       expect(find.text('Ingredients'), findsOneWidget);
-      expect(find.byType(ImageColumn), findsOneWidget);
-      expect(find.byType(IngridientColumn), findsOneWidget);
-      expect(find.byType(DescriptionColumn), findsOneWidget);
-      expect(find.byKey(const Key('imageColumn')), findsOneWidget);
-      expect(find.byKey(const Key('ingridientList')), findsOneWidget);
     
   });
 
-  testWidgets(
-    'should properly mock Image.network and not crash',
-    (WidgetTester tester) async {
-      mockNetworkImagesFor(() => tester.pumpWidget(
-        MaterialApp(
-        //home: RecipeDetailScreenWeb(recipe: null,),
-      ),
-      ));
-    },
-  );*/
 }
